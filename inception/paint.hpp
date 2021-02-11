4de285de9e6c7718e362acb638ea2bfe80946778
kilka-rav <zakirianov.rv@phystech.edu>
2021-02-11 11:53:03 +0300

    add handler_mouse

--- inception/paint.hpp
+++ inception/paint.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+//#include <SFML/Mouse.hpp>
 
 #define path_to_melody_one "/home/ravil/chat/data/sounds/sqwoz_woman.wav"
 #define path_to_fon "/home/ravil/chat/data/image/fon_memecria.jpg"
@@ -16,13 +17,13 @@ namespace screen {
             void play_sound();
         public:
             Background(int height, int width) : window(sf::VideoMode(height, width), "MEMECRIA") {}
-            //sf::RenderWindow create(int height, int width);
             void draw_on_window(const char path_to_image[]);
             void draw_on_window(const char path_to_image[], int x, int y);
             void draw_on_window(sf::Color colour);
             void handler_button();
             bool is_open() const;
             void display();
+            void enter_button(sf::Vector2i vec);
     };
     
     void Background::draw_on_window(const char path_to_image[]) {
@@ -56,6 +57,12 @@ namespace screen {
             else if ( event.type == sf::Event::KeyPressed ) {
                 play_sound();
             }
+            else if ( sf::Mouse::isButtonPressed(sf::Mouse::Left) ) {
+                std::cout << "LEFT CLICK" << std::endl;
+                sf::Vector2i position_mouse = sf::Mouse::getPosition(window);
+                //std::cout << position_mouse.x << "= x\n" << position_mouse.y << "= y\n" << std::endl;
+                enter_button(position_mouse);
+            }
         }
     }
 
@@ -66,18 +73,25 @@ namespace screen {
     void Background::display() {
         window.display();
     }
-    
+    //эти глобальные переменные необходимы для проигрывания саунда
+    //issue: add melody in global_variables
     sf::Sound ringthone_one;
     sf::SoundBuffer melody_one;
 
     void Background::play_sound() {
-        //sf::SoundBuffer melody_one;
         if ( !melody_one.loadFromFile(path_to_melody_one) ) {
             std::cout << "ERROR. Melody_one isn't detected" << std::endl;
         }
-        //saf::Sound ringthone_one;
         ringthone_one.setBuffer(melody_one);
         ringthone_one.play();
+    }
+
+    void Background::enter_button(sf::Vector2i position) {
+        float X = position.x;
+        float Y = position.y;
+        if ( (X > 0) & (Y > 0) & (X < 150) & (Y < 150) ) {
+            std::cout << "ENTER BUTTON\n";
+        }
     }
 }
 
