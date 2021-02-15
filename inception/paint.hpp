67ba58f0998036f9b7e4a68e78f13b4f25c05c76
GELGOOG-STUTZER <anton.okley@mail.ru>
2021-02-15 20:21:17 +0300

    Relative file path2

--- inception/paint.hpp
+++ inception/paint.hpp
@@ -6,9 +6,9 @@
 #include <vector>
 #include <iterator>
 
-#define path_to_melody_one "/home/ravil/chat/data/sounds/sp-cheers.wav"
-#define path_to_background "/home/ravil/chat/data/image/fon_memecria3.jpg"
-#define path_to_sound "/home/ravil/chat/data/image/icon_sound.jpg"
+#define path_to_melody_one "data/sounds/sp-cheers.wav"
+#define path_to_background "data/image/fon_memecria3.jpg"
+#define path_to_sound "data/image/icon_sound.jpg"
 
 //эти глобальные переменные необходимы для проигрывания саунда
 //issue: add melody in global_variables
