fa012ef11a1d0db01d4f6ac4c5ee9c5bf4764888
kilka-rav <zakirianov.rv@phystech.edu>
2021-02-19 23:14:22 +0300

    add field in class user and fix mistakes

--- encoder_decoder/user.hpp
+++ encoder_decoder/user.hpp
@@ -4,11 +4,12 @@ class User {
     public:
         std::string name;
         std::string family;
-        std::string informaion;
+        std::string information;
         std::string nickname;
         std::string avatar;
     private:                                //to keep this info in encode type
         std::string password;
         std::string unique_nickame;
         std::string number_phone;
+        std::string melody;
 }
