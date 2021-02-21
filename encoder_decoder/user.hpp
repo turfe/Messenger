e501e8c09310b689fa049f216ce8776b6ec17c97
GELGOOG-STUTZER <anton.okley@mail.ru>
2021-02-22 00:02:40 +0300

    Я сделаль

--- encoder_decoder/user.hpp
+++ encoder_decoder/user.hpp
@@ -1,14 +1,15 @@
 #pragma once
-
+#include <string>
 class User {
     public:
         std::string name;
         std::string information;
         std::string nickname;
         std::string avatar;
-    private:                                //to keep this info in encode type
+    private:                                  //to keep this info in encode type
+        std::string key;
         std::string password;
         std::string unique_nickame;
         std::string number_phone;
         std::string melody;
-}
+};
