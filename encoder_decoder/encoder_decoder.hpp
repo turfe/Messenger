c2f383b18d7738e79d7a9643e8a00ba72982b0e5
GELGOOG-STUTZER <anton.okley@mail.ru>
2021-02-28 23:09:04 +0300

    encoder_decoder

--- encoder_decoder/encoder_decoder.hpp
+++ encoder_decoder/encoder_decoder.hpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <sys/stat.h>
 
-
 void encoder(std::string &input, std::string &key, std::string &output) {
     int n = std::min(input.length(), key.length());
     for(int i = 0; i < n; i++) {
@@ -26,6 +25,28 @@ void encoder(std::string &input, std::string &key, std::string &output) {
     }
 }
 
+void decoder(std::string &input, std::string &key, std::string &output) {
+    int n = std::min(input.length(), key.length());
+    int shift = key[0] - '0';
+    for(int i = 0; i < n; i++) {
+        if(input[i] - shift < -128) {
+            input[i] = input[i] + 128 - shift;
+        }
+        else {
+            input[i] = input[i] - shift;
+        }
+    }
+    for(int i = 0; i <= (n / 2) - 1; i++) {
+        char temp;
+        temp = input[i];
+        input[i] = input[n - i - 1];
+        input[n - i - 1] = temp;
+    }
+    for(int i = 0; i < n; i++) {
+        output.push_back((-input[i]) ^ key[i]);
+    }
+}
+
 void file_unlock(char* pathname) {
     int status;
     status = chmod(pathname, 777);
@@ -40,31 +61,4 @@ void file_lock(char* pathname) {
 //    if(status != 0) {
 //        std::cout << "folder_lock ERROR" << '\n';
 //    }
-}
-
-int main() {
-    char pathname[] = "encoded_data/encoded_data.txt";
-    file_unlock(pathname);
-    std::ifstream fin("data.txt");
-    std::ofstream fout("encoded_data/encoded_data.txt");
-    if(!fin) return 1;
-    if(!fout) return 1;
-    std::string buffer, key, password, unique_nickname, number_phone, melody;
-    for(int i = 0; i < 4; i++) {
-        std::getline(fin, buffer);
-    }
-    std::getline(fin, key);
-    std::getline(fin, buffer);
-    encoder(buffer, key, password);
-    std::getline(fin, buffer);
-    encoder(buffer, key, unique_nickname);
-    std::getline(fin, buffer);
-    encoder(buffer, key, number_phone);
-    std::getline(fin, buffer);
-    encoder(buffer, key, melody);
-    fout << key << "\n"<< password << "\n" << unique_nickname << "\n" << number_phone << "\n" << melody << "\n";
-    
-    fout.close();
-    fin.close();
-    file_lock(pathname);
 }
